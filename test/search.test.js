2292cb60b7fa86b3822f0eca900b7b3bbda20861
Scott Ballantyne <ussballantyne@gmail.com>
2019-08-09 12:55:06 -0500

    bug fixed

--- test/search.test.js
+++ test/search.test.js
@@ -18,6 +18,11 @@ describe('SEC', function() {
       useCleanCache: true
     });
     var responses = {
+      "231cfbcbf15c957c9769fd4957e3b09c": read('html/search/browse-edgar?company=microsoft&myowner=exclude&action=getcompany'), 
+      "aef39b12f9273859a9a1b1edc4b67934": read('html/search/browse-edgar?company=google&myowner=exclude&action=getcompany'),
+      "368a1b3803a1ef7ef20618f79639e9c6": read('html/search/browse-edgar?company=uber&myowner=exclude&action=getcompany'),
+      "2868557a521af9c69b07fed4a97ffdea": read('html/search/browse-edgar?company=sap&myowner=exclude&action=getcompany'),
+      "57f56ec486acd249560655b0e9e1f370": read('html/search/browse-edgar?company=facebook&myowner=exclude&action=getcompany'),
       "378560c0db972c70043324cf1e97f7d8": read('html/search/browse-edgar?company=alliant&myowner=exclude&action=getcompany'),
       "10855892076611bc287353bbc4acb681": read('html/search/browse-edgar?company=alliant&myowner=exclude&action=getcompany&start=40&count=40&hidefilings=0'),
       'e14307d59f35fa65400cf8b8c915464a': read('html/search/browse-edgar?State=IA&myowner=exclude&action=getcompany'),
@@ -35,14 +40,68 @@ describe('SEC', function() {
 
   describe('BrowseEdgar', function() {
     describe('Search', function() { 
-     
+      it('{company: "uber"}', function(done) {
+        var search = new BrowseEdgar.search({});
+        search.query({
+          company: 'uber' 
+        }, function(err, results) {
+          var result = _.first(results);
+          assert.equal(results.length > 0, true);
+          done();
+        });
+      });
+
+      it('{company: "google"}', function(done) {
+        var search = new BrowseEdgar.search({});
+        search.query({
+          company: 'google' 
+        }, function(err, results) {
+          var result = _.first(results);
+          assert.equal(results.length > 0, true);
+          done();
+        });
+      });
+
+      it('{company: "microsoft"}', function(done) {
+        var search = new BrowseEdgar.search({});
+        search.query({
+          company: 'microsoft' 
+        }, function(err, results) {
+          var result = _.first(results);
+          assert.equal(results.length > 0, true);
+          done();
+        });
+      });
+
+      it('{company: "sap"}', function(done) {
+        var search = new BrowseEdgar.search({});
+        search.query({
+          company: 'sap' 
+        }, function(err, results) {
+          var result = _.first(results);
+          assert.equal(results.length > 0, true);
+          done();
+        });
+      });
+
+      it('{company: "facebook"}', function(done) {
+        var search = new BrowseEdgar.search({});
+        search.query({
+          company: 'facebook' 
+        }, function(err, results) {
+          var result = _.first(results);
+          assert.equal(results.length > 0, true);
+          done();
+        });
+      });
+
       it('{company: "alliant"}', function(done) {
         var search = new BrowseEdgar.search({});
         search.query({
           company: 'alliant' 
         }, function(err, results) {
           var result = _.first(_.where(results, {cik: '0001292450'}));
- 
+
           // console.log('alliant', results);
           assert.equal(result.filings, 'http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001292450&owner=include&count=40&hidefilings=0');
           assert.equal(result.state, 'Virginia');
@@ -76,7 +135,7 @@ describe('SEC', function() {
           State: 'IA',
         }, function(err, results) {
           var result = _.first(_.where(results, {cik: '0001267947'}));
- 
+
           // console.log('Iowa', results);
           assert.equal(result.filings, 'http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001267947&owner=include&count=40&hidefilings=0');
           assert.equal(result.state_code, "IA");
